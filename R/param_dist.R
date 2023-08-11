566c91202015dcb403e2a241b232081e6d1277fe
KeithJF82 <keith.fraser@imperial.ac.uk>
2023-08-11 13:08:49 +0100

    Create param_dist.R

---
+++ R/param_dist.R
@@ -0,0 +1,7 @@
+# Functions relating to processing of model calculations for multiple parameter sets
+#-------------------------------------------------------------------------------
+# TODO - Add function for getting median, mean, quartile etc. values for coefficients
+# of environmental covariates and/or FOI/R0 values
+#-------------------------------------------------------------------------------
+# TODO - Add function for doing global burden calculations (or similar) based on
+# multiple parameter sets and finding which sets give median, quartile etc. output
